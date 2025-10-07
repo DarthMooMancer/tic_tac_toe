0a07d776ce3a11f1588c70299b67e05da8916569
DarthMooMancer <107283522+DarthMooMancer@users.noreply.github.com>
2025-10-07 12:34:59 -0400

    input

--- src/window.cpp
+++ src/window.cpp
@@ -3,22 +3,6 @@
 #include <chrono>
 #include <thread>
 
-// void Window::terminate(Tile (&tile_list)[9], bool &running) {
-// 	if(tile_list[0]._delete && tile_list[0].m_symbol != ' '
-// 	&& tile_list[1]._delete && tile_list[1].m_symbol != ' '
-// 	&& tile_list[2]._delete && tile_list[2].m_symbol != ' '
-// 	&& tile_list[3]._delete && tile_list[3].m_symbol != ' '
-// 	&& tile_list[4]._delete && tile_list[4].m_symbol != ' '
-// 	&& tile_list[5]._delete && tile_list[5].m_symbol != ' '
-// 	&& tile_list[6]._delete && tile_list[6].m_symbol != ' '
-// 	&& tile_list[7]._delete && tile_list[7].m_symbol != ' '
-// 	&& tile_list[8]._delete && tile_list[8].m_symbol != ' ') {
-// 		std::cout << "Game Over! Tie" << std::endl;
-// 		running = false;
-//
-// 	}
-// }
-
 void Window::clear_display() {
 	for(int i = 0; i < ROW; i++) {
 		for(int j = 0; j < COL; j++) {
